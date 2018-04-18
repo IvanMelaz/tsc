48d9cea870679fce07132c055a5343eb68665bfa
ivan.melzani@beta80group.it <ivan.melzani@beta80group.it>
2018-04-18 20:45:12 +0200

    refactor to Spring boot Project

---
+++ tscSpringBootMVC/src/main/java/it/tsc/boot/WebMainClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package it.tsc.boot;
+
+/**
+ * @author "astraservice"
+ *
+ */
+public class WebMainClass {
+
+	/**
+	 * 
+	 */
+	public WebMainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
