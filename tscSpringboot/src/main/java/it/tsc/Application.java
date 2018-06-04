d89a18a32bf86bffef48c5eef017a2caed362e89
astraservice <astraservice@astraservice-X200MA>
2018-06-04 17:54:09 +0200

    log application bean; load Executor config

--- tscSpringboot/src/main/java/it/tsc/Application.java
+++ tscSpringboot/src/main/java/it/tsc/Application.java
@@ -2,6 +2,8 @@ package it.tsc;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(
-    basePackages = {"it.tsc.service", "it.tsc.repository", "it.tsc.config", "it.tsc.domain"})
+@ComponentScan(basePackages = {"it.tsc.controller", "it.tsc.config", "it.tsc.service",
+    "it.tsc.repository", "it.tsc.domain"})
 public class Application {
+  private static Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
@@ -22,12 +25,12 @@ public class Application {
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
     return args -> {
 
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
+      logger.debug("Let's inspect the beans provided by Spring Boot and application:");
 
       String[] beanNames = ctx.getBeanDefinitionNames();
       Arrays.sort(beanNames);
       for (String beanName : beanNames) {
-        System.out.println(beanName);
+        logger.debug(beanName);
       }
 
     };
