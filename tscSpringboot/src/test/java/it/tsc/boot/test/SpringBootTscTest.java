8cf80382dcd0b178c7d64a7a07298b3b1e937c3e
ivan.melzani@beta80group.it <ivan.melzani@beta80group.it>
2018-06-05 08:03:03 +0200

    fix first step , application working

--- tscSpringboot/src/test/java/it/tsc/boot/test/SpringBootTscTest.java
+++ tscSpringboot/src/test/java/it/tsc/boot/test/SpringBootTscTest.java
@@ -3,10 +3,20 @@
  */
 package it.tsc.boot.test;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import it.tsc.domain.User;
 
 /**
  * @author "astraservice"
@@ -15,6 +25,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootTscTest {
+
+	@PersistenceContext
+	private EntityManager em;
 
 	/**
 	 *
@@ -26,6 +39,15 @@ public class SpringBootTscTest {
 	@Test
 	public void testCreateClientSuccessfully() throws Exception {
 
+	}
+
+	@Test
+	public void userTest() {
+		TypedQuery<User> findQuery = em
+				.createNamedQuery(User.SELECT_BY_USERNAME, User.class);
+		findQuery.setParameter("username", "testUser");
+		List<User> allUsers = findQuery.getResultList();
+		assertNotNull(em);
 	}
 
 }
