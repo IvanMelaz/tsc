6458f28451cbcb8a7c03772068b094f683aa2153
ivan.melzani@beta80group.it <ivan.melzani@beta80group.it>
2018-10-04 00:59:55 +0200

    first commit spring cloud ha

---
+++ tscCloudService/src/main/java/it/tsc/cloud/service/CloudServiceApplication.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package it.tsc.cloud.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+/**
+ * @author astraservice
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudServiceApplication {
+
+	public static void main(String... args) {
+        SpringApplication.run(CloudServiceApplication.class, args);
+    }
+	
+    @RestController
+    class SimpleController {
+        @Value("${eureka.instance.metadataMap.zone}")
+        private String zone;
+        @GetMapping(value = "/zone", produces = APPLICATION_JSON_UTF8_VALUE)
+        public String zone() {
+            return "{\"zone\"=\"" + zone + "\"}";
+        }
+    }
+
+}
