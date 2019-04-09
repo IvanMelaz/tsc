59ce09b481a59657e9ae14fd150b1977fd0ea0f4
ivan.melzani@beta80group.it <ivan.melzani@beta80group.it>
2019-04-09 22:06:00 +0200

    commit add external test

--- tscSpringboot/src/main/java/it/tsc/config/WebAppConfig.java
+++ tscSpringboot/src/main/java/it/tsc/config/WebAppConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import it.tsc.component.CustomInterceptor;
 
 @Configuration
+@EnableWebSecurity
 public class WebAppConfig implements WebMvcConfigurer {
 	private static final Logger log = LoggerFactory
 			.getLogger(WebAppConfig.class);
+	
 	@Autowired
 	CustomInterceptor customInterceptor;
 
--- tscSpringboot/src/test/java/it/tsc/mvc/MvcSmartWatcherTest.java
+++ tscSpringboot/src/test/java/it/tsc/mvc/MvcSmartWatcherTest.java
@@ -3,6 +3,12 @@ package it.tsc.mvc;
  *
  */
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -28,6 +34,8 @@ import it.tsc.util.JsonUtil;
 public class MvcSmartWatcherTest extends MvcStandardTest {
 	private static Logger logger = LoggerFactory
 			.getLogger(MvcSmartWatcherTest.class);
+	
+	private final String USER_AGENT = "Mozilla/5.0";
 
 	/**
 	 *
@@ -47,6 +55,56 @@ public class MvcSmartWatcherTest extends MvcStandardTest {
 		MvcResult result = mvc.perform(builder).andExpect(ok).andReturn();
 		String content = result.getResponse().getContentAsString();
 		logger.debug("content {}", content);
+	}
+	
+	@Test
+	public void testStartAllarmExternalService() throws Exception {
+		sendPost();
+	}
+	
+	// HTTP POST request
+	private void sendPost() throws Exception {
+		String url = "http://www.mycare24.org:8080/startAlarm";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		
+		BaseSmartWatcher baseSmartWatcher = new BaseSmartWatcher();
+		baseSmartWatcher.setPhoneNumber("333255255");
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		con.setRequestProperty("Content-type", "application/json");
+		
+
+		String urlParameters = JsonUtil.getGsonConverter().toJson(baseSmartWatcher);
+		
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		//print result
+		System.out.println(response.toString());
+
 	}
 
 	@Ignore
