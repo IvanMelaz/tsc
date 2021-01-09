2dc7cac4897b61e324764b5104885a36e2180d57
IvanMelaz <ilmelaz@yahoo.it>
2021-01-09 12:54:10 +0100

    Update code [new token]

--- lotto/src/main/java/com/lotto/generator/Application.java
+++ lotto/src/main/java/com/lotto/generator/Application.java
@@ -45,7 +45,7 @@ public class Application {
 		CalculationStarter calculationStarter = ctx.getBean("calculationStarter", CalculationStarter.class);
 		calculationStarter.startCalculation();
 		return args -> {
-						log.info("Let's inspect the beans provided by Spring Boot and application:");
+						log.info("Print spring beans definitions:");
 						String[] beanNames = ctx.getBeanDefinitionNames();
 						Arrays.sort(beanNames);
 						for (String beanName : beanNames) {
