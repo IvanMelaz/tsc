d9124518ea0008800fe0a659e20d97372fc6e863
Ivan Melzani <i.melzani@be-tse.it>
2023-01-25 10:43:03 +0100

    Change tsc_telemedicare table

--- tscSpringboot/src/main/resources/data/create_table_telemedicare.sql
+++ tscSpringboot/src/main/resources/data/create_table_telemedicare.sql
@@ -5,11 +5,11 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 /*Table structure for table `tsc_telemedicare` */
 
 CREATE TABLE IF NOT EXISTS `tsc_telemedicare` (
-
-  `id_allarme` VARCHAR(50)  NOT NULL default '',
-  `ab_codi` VARCHAR(50)  NOT NULL default '',
-  `data_arrivo` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+    `ab_codi` VARCHAR(50)  NOT NULL default '',
+    `data_arrivo` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `id_allarme` VARCHAR(50)  NOT NULL default '',
+    `progressivo_allarme` VARCHAR(50)  NOT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS `tsc_user` (
     `role` VARCHAR(255)  NOT NULL default '',
